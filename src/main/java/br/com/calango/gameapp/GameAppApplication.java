b3145df79e160b56866a5867fcdaac9f2e737c78
landson90 <randelsilva1990@gmail.com>
2022-04-30 08:54:49 -0300

    first commit

---
+++ src/main/java/br/com/calango/gameapp/GameAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.calango.gameapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameAppApplication.class, args);
+	}
+
+}
